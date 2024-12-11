1f314f0f407f6f81ab70e58e01f41fca7fc636ea
Daniel Fichtinger <daniel@ficd.ca>
2024-12-11 16:56:55 -0500

    fixed styles

--- lua/ashen/util.lua
+++ lua/ashen/util.lua
@@ -6,9 +6,9 @@ M.hl = function(name, fg, bg, styles)
     fg = fg,
     bg = bg,
   }
-  -- local opt = vim.tbl_extend("error", t, styles or {})
-  -- vim.api.nvim_set_hl(0, name, opt)
-  vim.api.nvim_set_hl(0, name, t)
+  local opt = vim.tbl_extend("error", t, styles or {})
+  vim.api.nvim_set_hl(0, name, opt)
+  -- vim.api.nvim_set_hl(0, name, t)
 end
 
 function M.link_lang(table, lang)
