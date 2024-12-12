f604733924a2d1f0228e0a56f9f483eeb526e078
Daniel Fichtinger <daniel@ficd.ca>
2024-12-12 17:51:48 -0500

    added HL on yank, supports lazyvim

--- lua/ashen/init.lua
+++ lua/ashen/init.lua
@@ -1,16 +1,21 @@
 local M = {}
 
+M.lazyvim = pcall(require, "lazyvim")
+-- M.lazyvim = false
+
 -- Load the color scheme
 M.load = function()
   if vim.g.colors_name then
     vim.cmd("hi clear")
   end
+  -- vim.notify(tostring(M.lazyvim), vim.log.levels.DEBUG)
 
   vim.g.colors_name = "ashen"
   vim.o.termguicolors = true
   vim.api.nvim_command(string.format("set background=%s", "dark"))
   require("ashen.theme").setup()
   require("ashen.plugins").setup()
+  require("ashen.autocmds").setup()
 end
 
 M.setup = function(args)
