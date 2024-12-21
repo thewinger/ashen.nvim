cf6cfca2c5701e05c91d1c4e79c4ca51443dcd7d
Daniel Fichtinger <daniel@ficd.ca>
2024-12-21 16:03:11 -0500

    fix: Tweaked mini.icons colors

--- lua/ashen/plugins/mini-icons.lua
+++ lua/ashen/plugins/mini-icons.lua
@@ -5,11 +5,11 @@ M.map = {
   MiniIconsAzure = { c.blue },
   MiniIconsBlue = { c.blue_dark },
   MiniIconsCyan = { c.red_kindling },
-  MiniIconsGreen = { c.red_glowing },
+  MiniIconsGreen = { c.g_2 },
   MiniIconsOrange = { c.orange_blaze },
-  MiniIconsPurple = { c.orange_smolder },
+  MiniIconsPurple = { c.red_deep_ember },
   MiniIconsRed = { c.red_ember },
   MiniIconsYellow = { c.orange_glow },
-  MiniIconsGrey = { c.g_6 },
+  MiniIconsGrey = { c.g_2 },
 }
 return M
